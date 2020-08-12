fe0a93ad668366545737e5f61c481c64384fedd9
Hannah <jeong.pbzy@gmail.com>
2020-08-12 22:11:14 +0900

    Add files via upload

---
+++ Week2/int.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int age = get_int("What's your age?\n");
+    int days = age * 365;
+    printf("You are at least %i days old.\n", days);
+
+}
