7ae7bdd807f5770c49ac58847afcb0e2ba3fccd9
Hannah <jeong.pbzy@gmail.com>
2020-08-21 22:07:04 +0900

    Add files via upload

---
+++ Week5/compare-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    string s = get_string("s: ");
+    string t = get_string("t: ");
+
+    if (s == t)
+    {
+        printf("Same\n");
+    }
+    else
+    {
+       printf("Different\n"); 
+    }
+}
